5a19a8b90c18f9d6eea06e90da663c67135419b3
shubham shinde <93381750+shubhamshinde01@users.noreply.github.com>
2023-07-07 17:12:30 +0530

    Update code01.java

---
+++ 24apr/number system/code01.java
@@ -0,0 +1,15 @@
+2 | 5
+—----
+2 | 2 1
+—----
+2 | 1 0
+Reading numbers is reverse order gives us 101 as the binary representation
+2. Number 10
+2 | 10
+—-----
+2 | 5 0
+—-----
+2 | 2 1
+—-----
+2 | 1 0
+Reading in reverse order from bottom to top gives us 1010 as the binary representation of 10.
