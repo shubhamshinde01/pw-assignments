774ea5e4af2195500a10c2525a2e81d7a32a6261
shubham shinde <93381750+shubhamshinde01@users.noreply.github.com>
2023-07-07 17:07:32 +0530

    Update code04.java

---
+++ 24apr/searching/code04.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+public class Main{ 
+
+    public static void main(String args[]){
+    
+        int m;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to add : ");
+        m=sc.nextInt();      
+        
+        int []arr = new int[m];
+        
+        System.out.print("Enter the elements of the array: ");
+        
+        for(int i=0;i<m;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int element;
+        System.out.print("Enter the elements to be searched in array");
+        element = sc.nextInt();
+        int count=0;
+        
+
+        
+        for(int i=0;i<m;i++){
+            if(arr[i]==element){
+                idx = 1;
+                count++;
+            }
+        }
+
+       
+            System.out.println("occurance of target : "count);
+     
+       
+    }     
+}
