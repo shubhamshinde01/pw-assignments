921a9c1b75d46c62abe23c5e2acf07a55c09597c
shubham shinde <93381750+shubhamshinde01@users.noreply.github.com>
2023-07-07 17:14:12 +0530

    Update code02.java

---
+++ 24apr/number system/code02.java
@@ -0,0 +1,3 @@
+Q2. Number 15. Converting it in binary 1111, as its more than 1 set bits in the binary representation therefore it’s
+not power of 2.
+Number 32. Converting it to binary 10000, as it has only 1 set bit, therefore its power of 2.
